a1215486e8703b30618e17f493a4aebaad9aa6a3
DK013 <chakraborty.deep013@gmail.com>
2017-07-18 14:58:35 +0530

    initial functions

--- app.js
+++ app.js
@@ -20,6 +20,68 @@ server.post('https://zorn.herokuapp.com/api/messages', connector.listen());
 
 //incoming msg route
 bot.dialog('/', function (session, args) {
-    session.send("Hi");
-    console.log(session.message.text);
+    if (!session.userData.greeting) {
+        session.send("Hey. What's your name?");
+        session.userData.greeting = true;
+    } else if (!session.userData.name) {
+        getName(session);
+    } else if (!session.userData.email) {
+        getEmail(session);
+    } else if (!session.userData.password) {
+        getPassword(session);
+    } else {
+        session.userData = null;
+    }
+    session.endDialog();
 });
+
+//start session on name get
+function getName(session) {
+    name = session.message.text;
+    session.userData.name = name;
+    session.send("Hello, " + name + ". What is your Email ID?");
+}
+
+//validate user with email
+function getEmail(session) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    email = session.message.text;
+    if (re.test(email)) {
+        session.userData.email = email;
+        session.send("Thank you, " + session.userData.name + ". Please set a new password.");
+    } else {
+        session.send("Please type a valid email address. For example: test@hotmail.com");
+    }
+}
+//validate password
+function getPassword(session) {
+    var re = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+    password = session.message.text;
+    if (re.test(password)) {
+        session.userData.password = password;
+        var data = session.userData;
+        sendData(data, function (msg) {
+            session.send(msg);
+            session.userData = null;
+        });
+    } else {
+        session.send("Password must contain at least 8 characters, including at least 1 number, 1 uppercase letter, 1 lowercase letter and 1 special character. For example: Mybot@123");
+    }
+}
+
+//pipeline
+function sendData(data, cb) {
+    http.get("http://zorn.herokuapp.com/saveData.php?name=" + data.name + "&email=" + data.email + "&password=" + data.password, function (res) {
+        var msg = '';
+        res.on("data", function (chunk) {
+            msg += chunk;
+        });
+
+        res.on('end', function () {
+            cb(msg);
+        });
+
+    }).on('error', function (e) {
+        console.log("Got error: " + e.message);
+    });
+}
